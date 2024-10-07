f67c9f44894eefe2952d866378c84e506d32f08d
Aasif Ali <asif1510@gmail.com>
2024-10-07 15:47:25 +0530

    fixed landing

--- screens/landing.jsx
+++ screens/landing.jsx
@@ -1,26 +1,18 @@
 
 import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
-import {Image, StyleSheet, View} from 'react-native';
-import React from 'react';
+
+
 
 
 const LandingPage = () => {
   return (
     <View style={styles.container}>
       <View style={styles.bgImg}>
-
         <Image source={require("../assets/images/bg.png")} />
       </View>
       <View style={styles.contentContainer}>
-        <Text
-          style={styles.textHeading}
-        >FOOD
-          KART</Text>
-
-        <Image style={styles.img} source={require('../assets/images/bg.png')} />
-
+        <Text style={styles.textHeading}>FOOD KART</Text>
       </View>
     </View>
   );
@@ -37,22 +29,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
+  bgImg: {
     position: "absolute",
     top: 0,
-    right: 0
+    right: 0,
   },
-  textHeading: {
-    fontFamily: "OpenSans-Regular",
-    fontSize: 40,
-    fontWeight: "bold",
-    color: "#FA4A0C",
-    textAlign: "center"
+  contentContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 10
   }
-})
 
-    position: 'absolute',
-    top: 100,
-    right: 100,
-  },
 });
 
