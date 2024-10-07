e6699c1e0acf50aa8d20df21963e1578cbd94791
Aasif Ali <asif1510@gmail.com>
2024-10-07 15:16:26 +0530

    Added fonts

--- screens/landing.jsx
+++ screens/landing.jsx
@@ -1,13 +1,18 @@
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const LandingPage = () => {
   return (
     <View style={styles.container}>
       <View style={styles.bgImg}>
-        <Image style={styles.img} source={require("../assets/images/bg.png")} />
+        <Image source={require("../assets/images/bg.png")} />
       </View>
-      <View></View>
+      <View style={styles.contentContainer}>
+        <Text
+          style={styles.textHeading}
+        >FOOD
+          KART</Text>
+      </View>
     </View>
   )
 }
@@ -26,5 +31,12 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 0,
     right: 0
+  },
+  textHeading: {
+    fontFamily: "OpenSans-Regular",
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#FA4A0C",
+    textAlign: "center"
   }
 })
