403c1364665d4e1917f5239641f221c3ab3ee916
Waseem <waseemriyaz321@gmail.com>
2024-10-07 16:35:30 +0530

    feat: [Added signup screen]

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LandingPage from './screens/landing';
 import SignUpScreen from './screens/signup';
 import SigninScreen from './screens/signin';
@@ -10,30 +10,43 @@ const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer
-
-    >
+    <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          statusBarColor: "#000"
-        }}
-      >
-        <Stack.Screen name="landing" component={LandingPage} options={{
-          headerShown: false
-        }} />
-        <Stack.Screen name="signup" component={SignUpScreen} options={{
-          headerShown: false
-        }} />
-        <Stack.Screen name="signin" component={SigninScreen} options={{
-          headerShown: false
-        }} />
-        <Stack.Screen name="otp" component={OtpScreen} options={{
-          headerShown: false
-        }} />
+          statusBarColor: '#000',
+        }}>
+        <Stack.Screen
+          name="signup"
+          component={SignUpScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="landing"
+          component={LandingPage}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="signin"
+          component={SigninScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="otp"
+          component={OtpScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
