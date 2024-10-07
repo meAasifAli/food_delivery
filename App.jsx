1265ef72265984af4ac7589fc15d11cc7df825c3
Aasif Ali <asif1510@gmail.com>
2024-10-07 14:28:13 +0530

    Added auth Screens

--- App.jsx
+++ App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingPage from './screens/landing';
+import SignUpScreen from './screens/signup';
+import SigninScreen from './screens/signin';
+import OtpScreen from './screens/otp';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +12,18 @@ const App = () => {
   return (
     <NavigationContainer>
      <Stack.Navigator>
-        <Stack.Screen name="landing" component={LandingPage} />
+        <Stack.Screen name="landing" component={LandingPage} options={{
+          headerShown : false
+        }} />
+         <Stack.Screen name="signup" component={SignUpScreen} options={{
+          headerShown : false
+        }} />
+         <Stack.Screen name="signin" component={SigninScreen} options={{
+          headerShown : false
+        }} />
+        <Stack.Screen name="otp" component={OtpScreen} options={{
+          headerShown : false
+        }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
