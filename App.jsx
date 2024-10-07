642f9de5d0bc32be45875379e00db78491ac4d7e
Aasif Ali <asif1510@gmail.com>
2024-10-07 14:15:18 +0530

    added landing screen

--- App.jsx
+++ App.jsx
@@ -1,14 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingPage from './screens/landing';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+        <Stack.Screen name="landing" component={LandingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
