d9f093efc8a93fad006aed09ef27d79b5a4c1e26
Aasif Ali <asif1510@gmail.com>
2024-10-08 10:27:55 +0530

    feature/landing

--- screens/landing.jsx
+++ screens/landing.jsx
@@ -1,8 +1,9 @@
 
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import Button from '../components/Button';
 
 
-
+const { width, height } = Dimensions.get("window")
 
 
 const LandingPage = () => {
@@ -12,7 +13,43 @@ const LandingPage = () => {
         <Image source={require("../assets/images/bg.png")} />
       </View>
       <View style={styles.contentContainer}>
-        <Text style={styles.textHeading}>FOOD KART</Text>
+        <Text style={styles.primaryHeading}>FOOD KART</Text>
+        <Text style={styles.secondaryHeading}>Satisfy your cravings with just a tap
+          Order, Eat, Repeat!</Text>
+      </View>
+      <View style={{
+        position: "absolute",
+        bottom: 0,
+        backgroundColor: "#202020",
+        height: height * (192 / height),
+        width: width * (393 / width),
+        borderTopRightRadius: 50,
+        borderTopLeftRadius: 50,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#FA4A0C",
+            height: height * (64 / height),
+            width: width * (280 / width),
+            borderRadius: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <Text style={{
+            color: "#fff",
+            fontFamily: "Open-Sans",
+            fontWeight: "400",
+            fontSize: 32,
+            lineHeight: 43,
+            letterSpacing: 0.43,
+            textAlign: "center"
+          }}>Get Started</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -38,6 +75,25 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     gap: 10
+  },
+  primaryHeading: {
+    color: "#FA4A0C",
+    fontFamily: "Open-Sans",
+    fontWeight: "700",
+    fontSize: 48,
+    lineHeight: 65,
+    letterSpacing: 0.5,
+    textAlign: "center"
+  },
+  secondaryHeading: {
+    color: "#00030F",
+    fontFamily: "Open-Sans",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 25,
+    letterSpacing: 0.5,
+    textAlign: "center",
+    maxWidth: width * (290 / width)
   }
 
 });
